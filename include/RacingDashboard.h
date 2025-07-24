ab7e574ba11b651c6da2779b6c95258ea10527e2
meganechan <feadtodark@gmail.com>
2025-07-24 20:05:40 +0700

    Refactor RacingDashboard data handling: updated data parsing to use a comma-separated format, removed unused variables and UI elements, and optimized UI updates for speed, RPM, and gear, enhancing performance and clarity.

--- include/RacingDashboard.h
+++ include/RacingDashboard.h
@@ -47,19 +47,16 @@ class RacingDashboard {
 private:
 	int cellTitleHeight = 0;
 	bool hasReceivedData = true;
-	
 	// ตัวแปรสำหรับเก็บข้อมูลจาก SimHub
-	int speed = 0;
-	int rpm = 0;
-	int maxRpm = 8000;
-	float fuel = 0.0;
+	String speed = "0";
+	String rpm = "0";
+	int rpmPercent = 0; // percent 0-100
 	String gear = "N";
-	String lastLap = "00:00.000";
-	String nextLap = "00:00.000";
-	
 	// ตัวแปรสำหรับตรวจสอบว่ามีการอัพเดทข้อมูลใหม่หรือไม่
-	bool dataUpdated = false;
-	
+	bool speedDirty = false;
+	bool rpmDirty = false;
+	bool rpmPercentDirty = false;
+	bool gearDirty = false;
 	// LVGL UI objects
 	lv_obj_t *main_screen;
 	lv_obj_t *rpm_bar_container;
@@ -67,8 +64,6 @@ private:
 	lv_obj_t *gear_container;
 	lv_obj_t *speed_container;
 	lv_obj_t *rpm_container;
-	lv_obj_t *fuel_container;
-	lv_obj_t *lap_container;
 	lv_obj_t *gear_label;
 	lv_obj_t *gear_value_label;
 	lv_obj_t *speed_label;
@@ -77,23 +72,13 @@ private:
 	lv_obj_t *rpm_label;
 	lv_obj_t *rpm_value_label;
 	lv_obj_t *rpm_unit_label;
-	lv_obj_t *fuel_label;
-	lv_obj_t *fuel_value_label;
-	lv_obj_t *fuel_unit_label;
-	lv_obj_t *last_lap_label;
-	lv_obj_t *last_lap_value_label;
-	lv_obj_t *next_lap_label;
-	lv_obj_t *next_lap_value_label;
 	lv_obj_t *waiting_label;
-	
 	// สไตล์
 	lv_style_t style_container;
 	lv_style_t style_rpm_bar;
 	lv_style_t style_gear;
 	lv_style_t style_speed;
 	lv_style_t style_rpm;
-	lv_style_t style_fuel;
-	lv_style_t style_lap;
 	lv_style_t style_waiting;
 	
 public:
@@ -163,16 +148,6 @@ public:
 		lv_style_set_text_font(&style_rpm, &lv_font_montserrat_36);
 		lv_style_set_text_color(&style_rpm, lv_color_hex(0xFF8800)); // Orange
 		
-		// Fuel style
-		lv_style_init(&style_fuel);
-		lv_style_set_text_font(&style_fuel, &lv_font_montserrat_32);
-		lv_style_set_text_color(&style_fuel, lv_color_hex(0x00AAFF)); // Blue
-		
-		// Lap style
-		lv_style_init(&style_lap);
-		lv_style_set_text_font(&style_lap, &lv_font_montserrat_24);
-		lv_style_set_text_color(&style_lap, lv_color_white());
-		
 		// Waiting style
 		lv_style_init(&style_waiting);
 		lv_style_set_text_font(&style_waiting, &lv_font_montserrat_32);
@@ -241,70 +216,7 @@ public:
 		lv_label_set_text(rpm_value_label, "0");
 		lv_obj_add_style(rpm_value_label, &style_rpm, 0);
 		lv_obj_align(rpm_value_label, LV_ALIGN_CENTER, 0, 10);
-		
-		// RPM unit
-		rpm_unit_label = lv_label_create(rpm_container);
-		lv_label_set_text_fmt(rpm_unit_label, "/%d", maxRpm);
-		lv_obj_set_style_text_font(rpm_unit_label, &lv_font_montserrat_14, 0);
-		lv_obj_set_style_text_color(rpm_unit_label, lv_color_white(), 0);
-		lv_obj_align_to(rpm_unit_label, rpm_value_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 5);
-		
-		// Fuel container - ด้านบนขวา (ปรับตำแหน่งลงมา)
-		fuel_container = lv_obj_create(main_screen);
-		lv_obj_add_style(fuel_container, &style_container, 0);
-		lv_obj_set_size(fuel_container, 160, 100);
-		lv_obj_set_pos(fuel_container, 590, 220); // เพิ่ม Y position
-		
-		// Fuel title
-		fuel_label = lv_label_create(fuel_container);
-		lv_label_set_text(fuel_label, "FUEL");
-		lv_obj_set_style_text_font(fuel_label, &lv_font_montserrat_16, 0);
-		lv_obj_set_style_text_color(fuel_label, lv_color_hex(0x00AAFF), 0); // Blue
-		lv_obj_align(fuel_label, LV_ALIGN_TOP_MID, 0, 5);
-		
-		// Fuel value
-		fuel_value_label = lv_label_create(fuel_container);
-		lv_label_set_text(fuel_value_label, "0.0");
-		lv_obj_add_style(fuel_value_label, &style_fuel, 0);
-		lv_obj_align(fuel_value_label, LV_ALIGN_CENTER, 0, 5);
-		
-		// Fuel unit
-		fuel_unit_label = lv_label_create(fuel_container);
-		lv_label_set_text(fuel_unit_label, "L");
-		lv_obj_set_style_text_font(fuel_unit_label, &lv_font_montserrat_14, 0);
-		lv_obj_set_style_text_color(fuel_unit_label, lv_color_white(), 0);
-		lv_obj_align_to(fuel_unit_label, fuel_value_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 3);
-		
-		
-		// Lap container - ด้านล่าง (ปรับตำแหน่งลงมา)
-		lap_container = lv_obj_create(main_screen);
-		lv_obj_add_style(lap_container, &style_container, 0);
-		lv_obj_set_size(lap_container, 700, 150);
-		lv_obj_set_pos(lap_container, 50, 320); // เพิ่ม Y position
-		
-		// Last lap
-		last_lap_label = lv_label_create(lap_container);
-		lv_label_set_text(last_lap_label, "LAST LAP");
-		lv_obj_set_style_text_font(last_lap_label, &lv_font_montserrat_20, 0);
-		lv_obj_set_style_text_color(last_lap_label, lv_color_hex(0x00FFFF), 0); // Cyan
-		lv_obj_align(last_lap_label, LV_ALIGN_TOP_LEFT, 20, 10);
-		
-		last_lap_value_label = lv_label_create(lap_container);
-		lv_label_set_text(last_lap_value_label, "00:00.000");
-		lv_obj_add_style(last_lap_value_label, &style_lap, 0);
-		lv_obj_align_to(last_lap_value_label, last_lap_label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
-		
-		// Next lap
-		next_lap_label = lv_label_create(lap_container);
-		lv_label_set_text(next_lap_label, "NEXT LAP");
-		lv_obj_set_style_text_font(next_lap_label, &lv_font_montserrat_20, 0);
-		lv_obj_set_style_text_color(next_lap_label, lv_color_hex(0xFFFF00), 0); // Yellow
-		lv_obj_align(next_lap_label, LV_ALIGN_TOP_RIGHT, -20, 10);
-		
-		next_lap_value_label = lv_label_create(lap_container);
-		lv_label_set_text(next_lap_value_label, "00:00.000");
-		lv_obj_add_style(next_lap_value_label, &style_lap, 0);
-		lv_obj_align_to(next_lap_value_label, next_lap_label, LV_ALIGN_OUT_BOTTOM_RIGHT, 0, 10);
+		// Remove RPM unit label (maxRpm)
 		
 		// Waiting message (hidden initially)
 		waiting_label = lv_label_create(main_screen);
@@ -319,9 +231,6 @@ public:
 		lv_obj_add_flag(gear_container, LV_OBJ_FLAG_HIDDEN);
 		lv_obj_add_flag(speed_container, LV_OBJ_FLAG_HIDDEN);
 		lv_obj_add_flag(rpm_container, LV_OBJ_FLAG_HIDDEN);
-		lv_obj_add_flag(fuel_container, LV_OBJ_FLAG_HIDDEN);
-		lv_obj_add_flag(lap_container, LV_OBJ_FLAG_HIDDEN);
-		
 		// แสดง waiting message
 		lv_obj_clear_flag(waiting_label, LV_OBJ_FLAG_HIDDEN);
 	}
@@ -335,8 +244,6 @@ public:
 		lv_obj_clear_flag(gear_container, LV_OBJ_FLAG_HIDDEN);
 		lv_obj_clear_flag(speed_container, LV_OBJ_FLAG_HIDDEN);
 		lv_obj_clear_flag(rpm_container, LV_OBJ_FLAG_HIDDEN);
-		lv_obj_clear_flag(fuel_container, LV_OBJ_FLAG_HIDDEN);
-		lv_obj_clear_flag(lap_container, LV_OBJ_FLAG_HIDDEN);
 	}
 
 	// Called when new data is coming from computer
@@ -369,159 +276,96 @@ public:
 	
 	// Fast parsing function
 	void parseData(char* data) {
-		char* ptr = data;
-		char tempStr[16];
-		
-		// Parse format: "S:120;R:6500;MR:8000;F:45.5;G:4;L:01:23.456;N:01:20.123;"
-		while (*ptr) {
-			if (*ptr == 'S' && *(ptr+1) == ':') {
-				ptr += 2;
-				int i = 0;
-				while (*ptr && *ptr != ';' && i < 15) {
-					tempStr[i++] = *ptr++;
+		char* token;
+		int idx = 0;
+		token = strtok(data, ",");
+		while (token != NULL) {
+			switch (idx) {
+				case 0:
+					if (String(token) != speed) {
+						speed = String(token);
+						speedDirty = true;
+					}
+					break;
+				case 1:
+					if (String(token) != rpm) {
+						rpm = String(token);
+						rpmDirty = true;
+					}
+					break;
+				case 2: {
+					int newRpmPercent = atoi(token);
+					if (newRpmPercent != rpmPercent) {
+						rpmPercent = newRpmPercent;
+						rpmPercentDirty = true;
+					}
+					break;
 				}
-				tempStr[i] = '\0';
-				speed = atoi(tempStr);
+				case 3:
+					if (String(token) != gear) {
+						gear = String(token);
+						gearDirty = true;
+					}
+					break;
 			}
-			else if (*ptr == 'R' && *(ptr+1) == ':') {
-				ptr += 2;
-				int i = 0;
-				while (*ptr && *ptr != ';' && i < 15) {
-					tempStr[i++] = *ptr++;
-				}
-				tempStr[i] = '\0';
-				rpm = atoi(tempStr);
-			}
-			else if (*ptr == 'M' && *(ptr+1) == 'R' && *(ptr+2) == ':') {
-				ptr += 3;
-				int i = 0;
-				while (*ptr && *ptr != ';' && i < 15) {
-					tempStr[i++] = *ptr++;
-				}
-				tempStr[i] = '\0';
-				maxRpm = atoi(tempStr);
-				// อัพเดท RPM unit label
-				lv_label_set_text_fmt(rpm_unit_label, "/%d", maxRpm);
-			}
-			else if (*ptr == 'F' && *(ptr+1) == ':') {
-				ptr += 2;
-				int i = 0;
-				while (*ptr && *ptr != ';' && i < 15) {
-					tempStr[i++] = *ptr++;
-				}
-				tempStr[i] = '\0';
-				fuel = atof(tempStr);
-			}
-			else if (*ptr == 'G' && *(ptr+1) == ':') {
-				ptr += 2;
-				int i = 0;
-				while (*ptr && *ptr != ';' && i < 15) {
-					tempStr[i++] = *ptr++;
-				}
-				tempStr[i] = '\0';
-				gear = String(tempStr);
-			}
-			else if (*ptr == 'L' && *(ptr+1) == ':') {
-				ptr += 2;
-				int i = 0;
-				while (*ptr && *ptr != ';' && i < 15) {
-					tempStr[i++] = *ptr++;
-				}
-				tempStr[i] = '\0';
-				lastLap = String(tempStr);
-			}
-			else if (*ptr == 'N' && *(ptr+1) == ':') {
-				ptr += 2;
-				int i = 0;
-				while (*ptr && *ptr != ';' && i < 15) {
-					tempStr[i++] = *ptr++;
-				}
-				tempStr[i] = '\0';
-				nextLap = String(tempStr);
-			}
-			else {
-				ptr++;
-			}
+			idx++;
+			token = strtok(NULL, ",");
 		}
-		
 		// แสดงผลลัพธ์ที่ได้ใน Serial Monitor เพื่อตรวจสอบความถูกต้อง
 		Serial.print("Speed: ");
 		Serial.print(speed);
 		Serial.print(" km/h, RPM: ");
 		Serial.print(rpm);
-		Serial.print("/");
-		Serial.print(maxRpm);
-		Serial.print(", Fuel: ");
-		Serial.print(fuel);
-		Serial.print("L, Gear: ");
+		Serial.print(", RPM%: ");
+		Serial.print(rpmPercent);
+		Serial.print(", Gear: ");
 		Serial.print(gear);
-		Serial.print(", Last Lap: ");
-		Serial.print(lastLap);
-		Serial.print(", Next Lap: ");
-		Serial.println(nextLap);
-		
-		// ตั้งค่าให้อัพเดทจอ LCD
-		dataUpdated = true;
+		Serial.println();
 		hasReceivedData = true;
 	}
 
     void displayData() {
 		// Dashboard แสดงตลอดเวลา ไม่ต้องตรวจสอบ hasReceivedData แล้ว
-		
-		// คำนวณ RPM percentage
-		int rpmPercentage = (rpm * 100) / maxRpm;
-		if (rpmPercentage > 100) rpmPercentage = 100;
-		
-		// อัพเดท RPM bar
-		lv_bar_set_value(rpm_bar, rpmPercentage, LV_ANIM_OFF);
-		
-		// เปลี่ยนสี RPM bar ตาม percentage (racing style)
-		lv_color_t barColor;
-		if (rpmPercentage < 50) {
-			// เขียว (0-50%)
-			barColor = lv_color_hex(0x00FF00);
-		} else if (rpmPercentage < 70) {
-			// เหลือง (50-70%)
-			barColor = lv_color_hex(0xFFFF00);
-		} else if (rpmPercentage < 85) {
-			// ส้ม (70-85%)
-			barColor = lv_color_hex(0xFF8800);
-		} else {
-			// แดง (85-100%)
-			barColor = lv_color_hex(0xFF0000);
+		if (rpmPercentDirty) {
+			int percent = rpmPercent;
+			if (percent > 100) percent = 100;
+			if (percent < 0) percent = 0;
+			lv_bar_set_value(rpm_bar, percent, LV_ANIM_OFF);
+			lv_color_t barColor;
+			if (percent < 50) {
+				barColor = lv_color_hex(0x00FF00);
+			} else if (percent < 70) {
+				barColor = lv_color_hex(0xFFFF00);
+			} else if (percent < 85) {
+				barColor = lv_color_hex(0xFF8800);
+			} else {
+				barColor = lv_color_hex(0xFF0000);
+			}
+			lv_obj_set_style_bg_color(rpm_bar, barColor, LV_PART_INDICATOR);
+			rpmPercentDirty = false;
 		}
-		lv_obj_set_style_bg_color(rpm_bar, barColor, LV_PART_INDICATOR);
-		
-		// อัพเดท gear value
-		lv_label_set_text(gear_value_label, gear.c_str());
-		
-		// อัพเดท speed value
-		lv_label_set_text_fmt(speed_value_label, "%d", speed);
-		
-		// อัพเดท RPM value
-		lv_label_set_text_fmt(rpm_value_label, "%d", rpm);
-		
-		// อัพเดท fuel value
-		lv_label_set_text_fmt(fuel_value_label, "%.1f", fuel);
-		
-		// อัพเดท lap times
-		lv_label_set_text(last_lap_value_label, lastLap.c_str());
-		lv_label_set_text(next_lap_value_label, nextLap.c_str());
+		if (gearDirty) {
+			lv_label_set_text(gear_value_label, gear.c_str());
+			gearDirty = false;
+		}
+		if (speedDirty) {
+			lv_label_set_text(speed_value_label, speed.c_str());
+			speedDirty = false;
+		}
+		if (rpmDirty) {
+			lv_label_set_text(rpm_value_label, rpm.c_str());
+			rpmDirty = false;
+		}
+
     }
 
     void update() {
 		// อ่านข้อมูลจาก Serial
 		read();
-		
 		// อัพเดทจอ LCD เมื่อมีข้อมูลใหม่
-		if (dataUpdated) {
-			displayData();
-			dataUpdated = false;
-		}
-		
+		displayData();
 		// Dashboard จะแสดงตลอดเวลา - ไม่ต้องใช้ blinking animation อีกแล้ว
 		// Blinking animation ถูกปิดเพราะเราแสดง dashboard ตลอดเวลา
-		
 		// Handle LVGL tasks (เรียกครั้งเดียวต่อ update cycle)
 		lv_timer_handler();
     }
